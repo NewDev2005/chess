7adc78991c0183ae3d7a7a1b00b9d016470bdb42
krypto <clayjeansen3@gmail.com>
2025-10-01 00:50:28 +0530

    feat: create module: pieces, contains unicode value of all the chess pieces

---
+++ lib/chess_pieces.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Pieces
+  PIECES = { white_knight: "\u2658",
+             black_knight: "\u265E",
+             white_king: "\u2654",
+             black_king: "\u265A",
+             white_rook: "\u2656",
+             black_rook: "\u265C",
+             white_bishop: "\u2657",
+             black_bishop: "\u265D",
+             white_queen: "\u2655",
+             black_queen: "\u265B",
+             black_pawn: "\u265F",
+             white_pawn: "\u2659" }.freeze
+end
