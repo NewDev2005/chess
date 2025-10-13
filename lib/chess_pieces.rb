f6672d3b6497591917b35e0514dfa9265eb12370
krypto <clayjeansen3@gmail.com>
2025-10-14 00:09:25 +0530

    update: replace PIECES with Black_pieces and white_pieces contains the unicode value of the chess pieces of the respective colors

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 module Pieces
-  PIECES = { white_knight: "\u2658",
-             black_knight: "\u265E",
-             white_king: "\u2654",
-             black_king: "\u265A",
-             white_rook: "\u2656",
-             black_rook: "\u265C",
-             white_bishop: "\u2657",
-             black_bishop: "\u265D",
-             white_queen: "\u2655",
-             black_queen: "\u265B",
-             black_pawn: "\u265F",
-             white_pawn: "\u2659" }.freeze
+  BLACK_PIECES = {
+    rook: "\u265C",
+    knight: "\u265E",
+    bishop: "\u265D",
+    queen: "\u265B",
+    king: "\u265A",
+    pawn: "\u265F"
+  }.freeze
+
+  WHITE_PIECES = {
+    rook: "\u2656",
+    knight: "\u2658",
+    bishop: "\u2657",
+    queen: "\u2655",
+    king: "\u2654",
+    pawn: "\u2659"
+  }.freeze
 end
