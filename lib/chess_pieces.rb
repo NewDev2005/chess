6ddb8f8d57af65a33cee147ee43eb4beb97acc97
krypto <clayjeansen3@gmail.com>
2025-10-18 12:48:16 +0530

    feat: add white afer the unicode to centre the pieces

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -2,20 +2,28 @@
 
 module Pieces
   BLACK_PIECES = {
-    rook: "\u265C",
-    knight: "\u265E",
-    bishop: "\u265D",
-    queen: "\u265B",
-    king: "\u265A",
-    pawn: "\u265F"
+    rook: "\u265C ",
+    knight: "\u265E ",
+    bishop: "\u265D ",
+    queen: "\u265B ",
+    king: "\u265A ",
+    bishop2: "\u265D ",
+    knight2: "\u265E ",
+    rook2: "\u265C "
   }.freeze
 
+  BLACK_PAWN = "\u265F "
+
   WHITE_PIECES = {
-    rook: "\u2656",
-    knight: "\u2658",
-    bishop: "\u2657",
-    queen: "\u2655",
-    king: "\u2654",
-    pawn: "\u2659"
+    rook: "\u2656 ",
+    knight: "\u2658 ",
+    bishop: "\u2657 ",
+    queen: "\u2655 ",
+    king: "\u2654 ",
+    bishop2: "\u2657 ",
+    knight2: "\u2658 ",
+    rook2: "\u2656 "
   }.freeze
+
+  WHITE_PAWN = "\u2659 "
 end
