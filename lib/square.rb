3687a50db82d31be6422318206cc9c5ce827ba57
krypto <clayjeansen3@gmail.com>
2025-10-18 12:46:19 +0530

    fix: spacing of the empty square

--- lib/square.rb
+++ lib/square.rb
@@ -1,24 +1,20 @@
 # frozen_string_literal: true
 
 require_relative 'color'
-
+require_relative 'chess_pieces'
 class Square # rubocop:disable Style/Documentation
   using Color
-  def initialize(color, piece = '', piece_color = nil)
+  def initialize(color, piece = '  ', piece_color = nil)
     @color = color
     @piece = piece
     @piece_color = piece_color
   end
 
   def to_s
-    if @piece == ''
+    if @piece == '  '
       @piece.bg_color(@color)
-    elsif @piece != ''
+    elsif @piece != '  '
       @piece.bg_color(@color).fg_color(@piece_color)
     end
   end
 end
-
-
-puts Square.new(:light_color).to_s
-# puts sqr.to_s
