c3a93c8411f10d0f615cf5aee3dd6b5d8a4728f6
krypto <clayjeansen3@gmail.com>
2025-10-23 23:28:27 +0530

    add instruction #choose_piece_message and #select_sqr_to_move

--- lib/instruction.rb
+++ lib/instruction.rb
@@ -4,5 +4,12 @@ module GameInstruction # rubocop:disable Style/Documentation
   def enter_name_message(player)
     print "#{player} enter your name before proceeding in the game!:  "
   end
+  
+  def choose_piece_message(name)
+    puts "#{name}: Choose a piece you  wish to move"
+  end
 
+  def select_sqr_to_move
+    puts 'Cool, now select a square where you would like to move your piece'
+  end
 end
