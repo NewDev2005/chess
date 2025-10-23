b1ba24fc06a0d36b6abcceef94041c5a95b1b8dc
krypto <clayjeansen3@gmail.com>
2025-10-23 23:27:37 +0530

    feat(#start): functionality to run the game in loop

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -3,9 +3,11 @@
 require_relative 'board'
 require_relative 'player'
 require_relative 'instruction'
+require_relative 'game_logic'
 
 class PlayGame # rubocop:disable Style/Documentation
   include GameInstruction
+  include GameLogic
   def initialize(board = Board.new)
     @board = board
     @player1 = Player.new
@@ -32,10 +34,30 @@ class PlayGame # rubocop:disable Style/Documentation
     end
   end
 
-  def start
-    assign_color_to_players_randomly
+  def register_move(player_obj)
+    choose_piece_message(player_obj.name)
+    player_obj.select_piece = player_obj.prompt
+    select_sqr_to_move
+    player_obj.select_sqr_to_place = player_obj.prompt
+    move_pieces(@board.board, player_obj.select_piece, player_obj.select_sqr_to_place)
+    @board.display_board
+  end
+
+  def game_loop
     loop do
-      
+      register_move(@player1)
+      register_move(@player2)
     end
   end
+
+  def start
+    assign_color_to_players_randomly
+    @board.set_pieces
+    @board.display_board
+    game_loop
+  end
 end
+
+# board = Board.new
+# board.set_pieces
+# board.display_board
