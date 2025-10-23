15f0d2ba064157e5b34c798be73c54aa52190e77
krypto <clayjeansen3@gmail.com>
2025-10-23 10:21:45 +0530

    create module for storing log messages

---
+++ lib/instruction.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module GameInstruction # rubocop:disable Style/Documentation
+  def enter_name_message(player)
+    print "#{player} enter your name before proceeding in the game!:  "
+  end
+
+end
