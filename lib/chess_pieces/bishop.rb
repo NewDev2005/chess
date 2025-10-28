1bb19bc9b89d46422bb974c8b5643aae927d6e32
krypto <clayjeansen3@gmail.com>
2025-10-28 18:04:08 +0530

    fix: invalid moves

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -19,29 +19,51 @@ class Bishop # rubocop:disable Style/Documentation
   end
 
   def movement
-    up_right_diagonal_moves(@current_position)
-    down_right_diagonal_moves(@current_position)
+    p up_right_diagonal_moves(@current_position)
+    p down_right_diagonal_moves(@current_position)
+    p up_left_diagonal_moves(@current_position)
+    p down_left_diagonal_moves(@current_position)
   end
 
   def up_right_diagonal_moves(current_position)
-    current_position = split_chr_and_num(current_position)
     possible_moves = []
     loop do
-      possible_moves.push("#{(current_position[0].ord + 1).chr}#{current_position[1].to_i + 1}")
-      break if possible_moves.last.end_with?('8') || possible_moves.last.start_with?('h')
+      break if current_position.end_with?('8') || current_position.start_with?('h')
 
+      possible_moves.push("#{(current_position[0].ord + 1).chr}#{current_position[1].to_i + 1}")
       current_position = possible_moves.last
     end
     possible_moves
   end
 
   def down_right_diagonal_moves(current_position)
-    current_position = split_chr_and_num(current_position)
     possible_moves = []
     loop do
-      possible_moves.push("#{(current_position[0].ord + 1).chr}#{current_position[1].to_i - 1}")
-      break if possible_moves.last.end_with?('1') || possible_moves.last.start_with?('h')
+      break if current_position.end_with?('1') || current_position.start_with?('h')
 
+      possible_moves.push("#{(current_position[0].ord + 1).chr}#{current_position[1].to_i - 1}")
+      current_position = possible_moves.last
+    end
+    possible_moves
+  end
+
+  def up_left_diagonal_moves(current_position)
+    possible_moves = []
+    loop do
+      break if current_position.end_with?('8') || current_position.start_with?('a')
+
+      possible_moves.push("#{(current_position[0].ord - 1).chr}#{current_position[1].to_i + 1}")
+      current_position = possible_moves.last
+    end
+    possible_moves
+  end
+
+  def down_left_diagonal_moves(current_position)
+    possible_moves = []
+    loop do
+      break if current_position.end_with?('1') || current_position.start_with?('a')
+
+      possible_moves.push("#{(current_position[0].ord - 1).chr}#{current_position[1].to_i - 1}")
       current_position = possible_moves.last
     end
     possible_moves
@@ -51,3 +73,6 @@ class Bishop # rubocop:disable Style/Documentation
     coord.split('')
   end
 end
+
+bishop = Bishop.new(:white, 'd4')
+bishop.movement
