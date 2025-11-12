dbb0a9823775cf321ea52595e5c44c482c1e577b
krypto <clayjeansen3@gmail.com>
2025-11-13 00:15:23 +0530

    feat(movement): gives possible moves relative to the current position

---
+++ lib/chess_pieces/knight.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class Knight # rubocop:disable Style/Documentation
+  attr_reader :possible_moves
+
+  def initialize(color, current_position)
+    @color = color
+    @current_position = current_position
+    @possible_moves = []
+  end
+
+  def movement
+    get_possible_moves(@current_position)
+    @possible_moves
+  end
+
+  private
+
+  def valid_move?(move)
+    return if move.length > 2
+
+    if move[0].ord >= 97 && move[0].ord <= 104 && move[1].to_i >= 1 && move[1].to_i <= 8
+      true
+    else
+      false
+    end
+  end
+
+  def get_possible_moves(current_position)
+    double_increment(current_position, 2, 1)
+    double_increment(current_position, 1, 2)
+    decrement_increment(current_position, 1, 2)
+    decrement_increment(current_position, 2, 1)
+    increment_decrement(current_position, 2, 1)
+    decrement_decrement(current_position, 1, 2)
+    decrement_decrement(current_position, 2, 1)
+    increment_decrement(current_position, 1, 2)
+  end
+
+  def double_increment(move, num1, num2)
+    return unless valid_move?("#{(move[0].ord + num1).chr}#{move[1].to_i + num2}")
+
+    @possible_moves.push("#{(move[0].ord + num1).chr}#{move[1].to_i + num2}")
+  end
+
+  def decrement_increment(move, num1, num2)
+    return unless valid_move?("#{(move[0].ord - num1).chr}#{move[1].to_i + num2}")
+
+    @possible_moves.push("#{(move[0].ord - num1).chr}#{move[1].to_i + num2}")
+  end
+
+  def increment_decrement(move, num1, num2)
+    return unless valid_move?("#{(move[0].ord + num1).chr}#{move[1].to_i - num2}")
+
+    @possible_moves.push("#{(move[0].ord + num1).chr}#{move[1].to_i - num2}")
+  end
+
+  def decrement_decrement(move, num1, num2)
+    return unless valid_move?("#{(move[0].ord - num1).chr}#{move[1].to_i - num2}")
+
+    @possible_moves.push("#{(move[0].ord - num1).chr}#{move[1].to_i - num2}")
+  end
+end
