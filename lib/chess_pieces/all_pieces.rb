7d4535a6c43f125d0605507acad878d80dda2a19
krypto <clayjeansen3@gmail.com>
2025-11-14 01:14:42 +0530

    create file to store the references of other files(pieces)

---
+++ lib/chess_pieces/all_pieces.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'bishop'
+require_relative 'rook'
+require_relative 'knight'
+require_relative 'king'
+require_relative 'queen'
+require_relative 'pawn'
