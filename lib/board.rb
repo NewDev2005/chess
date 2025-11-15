681e94d911735548ecc9c8567927a65c00c1f370
krypto <clayjeansen3@gmail.com>
2025-11-15 14:25:02 +0530

    fix bug wrong indexing in #create_instances of white/black pawn

--- lib/board.rb
+++ lib/board.rb
@@ -5,7 +5,7 @@ require_relative 'chess_pieces_unicode'
 require_relative 'game_logic'
 require_relative 'chess_pieces/all_pieces'
 
-class Board # rubocop:disable Style/Documentation
+class Board # rubocop:disable Style/Documentation,Metrics/ClassLength
   attr_accessor :board
 
   include Pieces
@@ -48,37 +48,46 @@ class Board # rubocop:disable Style/Documentation
   private
 
   def create_instances_of_pieces(arr, sqr_color1, sqr_color2, piece_color) # rubocop:disable Metrics/AbcSize
-    arr.push('a' => Square.new(sqr_color1, Rook.new(sqr_color1, piece_color)))
-    arr.push('b' => Square.new(sqr_color2, Knight.new(sqr_color2, piece_color)))
-    arr.push('c' => Square.new(sqr_color1, Bishop.new(sqr_color1, piece_color)))
-    arr.push('d' => Square.new(sqr_color2, Queen.new(sqr_color2, piece_color)))
-    arr.push('e' => Square.new(sqr_color1, King.new(sqr_color1, piece_color)))
-    arr.push('f' => Square.new(sqr_color2, Bishop.new(sqr_color2, piece_color)))
-    arr.push('g' => Square.new(sqr_color1, Knight.new(sqr_color1, piece_color)))
-    arr.push('h' => Square.new(sqr_color2, Rook.new(sqr_color2, piece_color)))
+    arr.push('a' => Square.new(sqr_color1, Rook.new(piece_color)))
+    arr.push('b' => Square.new(sqr_color2, Knight.new(piece_color)))
+    arr.push('c' => Square.new(sqr_color1, Bishop.new(piece_color)))
+    arr.push('d' => Square.new(sqr_color2, Queen.new(piece_color)))
+    arr.push('e' => Square.new(sqr_color1, King.new(piece_color)))
+    arr.push('f' => Square.new(sqr_color2, Bishop.new(piece_color)))
+    arr.push('g' => Square.new(sqr_color1, Knight.new(piece_color)))
+    arr.push('h' => Square.new(sqr_color2, Rook.new(piece_color)))
   end
 
   def create_instances_of_black_pawns(arr, piece_color)
-    (1..8).each do |i|
-      arr.push(@alphabetic_coords[i] => Square.new(color_in_odd_rank(i), Pawn.new(color_in_odd_rank(i), piece_color)))
+    num = 1
+    (0..7).each do |i|
+      arr.push(@alphabetic_coords[i] => Square.new(color_in_odd_rank(num),
+                                                   Pawn.new(piece_color)))
+      num += 1
     end
   end
 
   def create_instances_of_white_pawns(arr, piece_color)
-    (1..8).each do |i|
-      arr.push(@alphabetic_coords[i] => Square.new(color_in_even_rank(i), Pawn.new(color_in_even_rank(i), piece_color)))
+    num = 1
+    (0..7).each do |i|
+      arr.push(@alphabetic_coords[i] => Square.new(color_in_even_rank(num), Pawn.new(piece_color)))
+      num += 1
     end
   end
 
   def create_instances_of_squares_in_even_rank(arr)
-    (1..8).each do |i|
-      arr.push(@alphabetic_coords[i] => Square.new(color_in_even_rank(i)))
+    num = 1
+    (0..7).each do |i|
+      arr.push(@alphabetic_coords[i] => Square.new(color_in_even_rank(num)))
+      num += 1
     end
   end
 
   def create_instances_of_squares_in_odd_ranks(arr)
-    (1..8).each do |i|
-      arr.push(@alphabetic_coords[i] => Square.new(color_in_odd_rank(i)))
+    num = 1
+    (0..7).each do |i|
+      arr.push(@alphabetic_coords[i] => Square.new(color_in_odd_rank(num)))
+      num += 1
     end
   end
 
