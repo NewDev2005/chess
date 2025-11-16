47b25c593567b159fcd7b014b1d7fce8e8b4772e
krypto <clayjeansen3@gmail.com>
2025-11-16 23:31:45 +0530

    update current position of the piece after each move

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -22,17 +22,17 @@ module GameLogic # rubocop:disable Style/Documentation
     board.each do |rank_num, rank|
       next unless rank_num == destination[1]
 
-      update_destination(rank, destination[0], sqr_propertities)
+      update_destination(rank, destination, sqr_propertities)
     end
   end
 
   def update_destination(rank, destination, sqr_propertities)
     rank.each do |elem|
       elem.each do |alphabetic_coord, sqr|
-        next unless alphabetic_coord == destination
+        next unless alphabetic_coord == destination[0]
 
         sqr.piece = sqr_propertities[0]
-        sqr.piece_color = sqr_propertities[1]
+        sqr.piece.update_current_position(destination)
       end
     end
   end
@@ -43,9 +43,8 @@ module GameLogic # rubocop:disable Style/Documentation
       elem.each do |alphabetic_coord, sqr|
         next unless alphabetic_coord == origin
 
-        origin_sqr_propertities.push(sqr.piece).push(sqr.piece_color)
+        origin_sqr_propertities.push(sqr.piece)
         sqr.piece = '  '
-        sqr.piece_color = nil
       end
     end
     origin_sqr_propertities
