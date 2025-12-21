cf8ea21152f63d1555ee8e054d26edeec8b6c6d2
krypto <clayjeansen3@gmail.com>
2025-12-22 00:17:55 +0530

    prevent players from making in-accurate move

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -37,20 +37,60 @@ class PlayGame # rubocop:disable Style/Documentation
   end
 
   def register_move(player_obj)
-    choose_piece_message(player_obj.name)
-    # player_obj.select_piece = player_obj.prompt # extract this line into a method
+    choose_piece_message(player_obj)
     select_piece(player_obj)
-    select_sqr_to_move
-    player_obj.select_sqr_to_place = player_obj.prompt
+    select_sqr_to_move_instruction
+    select_sqr_to_place_move(player_obj)
     move_pieces(@board.board, player_obj.select_piece, player_obj.select_sqr_to_place)
     @board.display_board
   end
 
   def select_piece(player)
     player.select_piece = player.prompt
+    verify_player_color_pick(player)
     @game_features.mark_valid_moves_of_selected_piece(@board.board, player.select_piece)
     @board.display_board
-    # unmark_the_valid_moves_of_selected_piece(@board.board, player.select_piece, 'unmark')
+    @game_features.unmark_the_marked_sqr
+  end
+
+  def select_sqr_to_place_move(player)
+    player.select_sqr_to_place = player.prompt
+    check_for_legal_move(player)
+  end
+
+  def check_for_legal_move(player)
+    until accurate_move?(player.select_sqr_to_place)
+      invalid_move_for_piece_message(player.select_sqr_to_place)
+      player.select_sqr_to_place = player.prompt
+    end
+  end
+
+  def accurate_move?(move)
+    @game_features.valid_moves.include?(move)
+  end
+
+  def verify_player_color_pick(player)
+    until accurate_color_selection?(player)
+      puts 'Choose your own piece XD'
+      player.select_piece = player.prompt
+    end
+  end
+
+  def accurate_color_selection?(player_obj)
+    piece_color = get_piece_color(player_obj.select_piece)
+    piece_color == player_obj.color_pick
+  end
+
+  def get_piece_color(coord)
+    @board.board.each do |rank_num, files|
+      next unless rank_num == coord[1]
+
+      files.each do |elem|
+        elem.each do |alphabetic_coord, sqr|
+          return sqr.piece.fg_color if alphabetic_coord == coord[0]
+        end
+      end
+    end
   end
 
   def game_loop
