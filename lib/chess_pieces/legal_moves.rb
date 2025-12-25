21a7fa0b601b1fe9bac16531232176fe8ef51849
krypto <clayjeansen3@gmail.com>
2025-12-26 00:40:51 +0530

    create module LegalMOves to genetate legal moves of a piece

---
+++ lib/chess_pieces/legal_moves.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# retrieves the legal moves for a piece
+
+module LegalMoves # rubocop:disable Style/Documentation
+  def verify_legal_moves(board, coord, color)
+    legal_move = nil
+    board.each do |rank, files|
+      next unless rank == coord[1]
+
+      files.each do |elem|
+        elem.each do |alphabetic_coord, sqr|
+          legal_move = coord if alphabetic_coord == coord[0] && sqr.piece == '  '
+
+          legal_move = coord if alphabetic_coord == coord[0] && sqr.piece != '  ' && sqr.piece.fg_color != color
+        end
+      end
+    end
+    legal_move
+  end
+
+  def get_the_sqr_obj(board, coord)
+    board.each do |rank_num, files|
+      next unless rank_num == coord[1]
+
+      files.each do |elem|
+        elem.each do |alphabetic_coord, sqr|
+          return sqr if alphabetic_coord == coord[0]
+        end
+      end
+    end
+  end
+
+  def sqr_is_occupied?(board, coord)
+    sqr = get_the_sqr_obj(board, coord)
+    return true if sqr.piece != '  '
+
+    false
+  end
+end
