aca3d98f11ff4ccd3e03de13b96ae46c5e73e2eb
krypto <clayjeansen3@gmail.com>
2026-01-14 12:59:09 +0530

    fix bug

--- lib/check.rb
+++ lib/check.rb
@@ -1,34 +1,55 @@
 # frozen_string_literal: true
 
 require_relative 'retrieve_pieces'
+require_relative 'game_logic'
 
 class Check # rubocop:disable Style/Documentation
   include PieceRetrieval
+  include GameLogic
+
   def initialize(board)
     @pieces = nil
-    @board = board.board
-    @opponent_color = nil
+    @board = board
+    @color = nil
+    @number_of_check = nil
   end
 
-  def in_check?(opponent_color)
-    @opponent_color = opponent_color
-    @pieces = retrieve_pieces(@board, opponent_color)
-    iterate_through_pieces(@pieces)
+  def in_check?(color, board)
+    @number_of_check = 0
+    @color = color
+    @pieces = retrieve_pieces(board, @color)
+    iterate_through_pieces(@pieces, board)
+    return true if @number_of_check.positive?
+
+    false
   end
 
-  def iterate_through_pieces(pieces)
+  # def prompt_user_to_escape_check(origin, destination)
+  #   cloned_board = @board.clone
+  #   move_pieces(cloned_board, origin, destination)
+  # end
+
+  private
+
+  def iterate_through_pieces(pieces, board)
     pieces.each do |piece|
-      next unless piece.get_legal_moves(@board).empty? == false
+      if piece.instance_of?(Pawn) && piece.legal_capture_move(board).empty? == false
+        @number_of_check += 1 if found_king?(piece.legal_capture_move(board), board)
+      end
 
-      found_king?(piece.get_legal_moves(@board)) # break once the opponent king is found!
+      next unless piece.get_legal_moves(board).empty? == false && piece.instance_of?(Pawn) == false
+
+      @number_of_check += 1 if found_king?(piece.get_legal_moves(board), board)
     end
   end
 
-  def found_king?(legal_moves)
+  def found_king?(legal_moves, board)
     legal_moves.each do |legal_move|
-      sqr = get_the_sqr_obj(@board, legal_move)
-      return true if sqr.piece != '  ' && sqr.piece.instance_of?(King) && sqr.piece.fg_color == @color
+      sqr = get_the_sqr_obj(board, legal_move)
+      next if sqr.piece == '  '
+      return true if sqr.piece != '  ' && sqr.piece.instance_of?(King) && sqr.piece.fg_color != @color
     end
+    false
   end
 
   def get_the_sqr_obj(board, coord)
