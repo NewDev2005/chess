31098ea9c40cf2f5d8b33004d901c04a3d433d71
krypto <clayjeansen3@gmail.com>
2026-01-17 17:09:28 +0530

    #check_mate: declares check-mate when there is no legal move to escape the check

--- lib/check.rb
+++ lib/check.rb
@@ -8,23 +8,66 @@ class Check # rubocop:disable Style/Documentation
   include GameLogic
 
   def initialize(board)
-    @pieces = nil
     @board = board
-    @color = nil
+    @color = nil # color of the piece delivering the check(opponent piece)
+    @player_color = nil # color of the piece whose king is in check
     @number_of_check = nil
+    @move_to_escape_check = 0
   end
 
   def in_check?(color, board)
     @number_of_check = 0
-    @color = color
-    @pieces = retrieve_pieces(board, @color)
-    iterate_through_pieces(@pieces, board)
+    @color = alter_color(color)
+    pieces = retrieve_pieces(board, @color)
+    iterate_through_pieces(pieces, board)
     return true if @number_of_check.positive?
 
     false
   end
 
+  def check_mate?(color, board)
+    @player_color = color
+    player_pieces = retrieve_pieces(board, color)
+    check_piece_legal_moves(player_pieces, board)
+    return true if @move_to_escape_check.zero?
+
+    false
+  end
+
+  # illegal_move: any move that puts one's king in check
+  def illegal_move?(origin, destination)
+    cloned_board = Marshal.load(Marshal.dump(@board))
+    piece_color = get_the_sqr_obj(cloned_board).piece.fg_color
+    move_pieces(cloned_board.board, origin, destination)
+    return true if in_check?(piece_color, cloned_board.board)
+
+    false
+  end
+
   private
+
+  def check_piece_legal_moves(pieces, board)
+    pieces.each do |piece|
+      next if piece.get_legal_moves(board).empty?
+
+      iterate_legal_moves(piece, board)
+    end
+  end
+
+  def iterate_legal_moves(piece, board)
+    legal_moves = piece.get_legal_moves(board)
+    legal_moves.each do |legal_move|
+      @move_to_escape_check += 1 if escape_check?(piece.current_position, legal_move)
+    end
+  end
+
+  def escape_check?(origin, destination)
+    cloned_board = Marshal.load(Marshal.dump(@board))
+    move_pieces(cloned_board.board, origin, destination)
+    return true if in_check?(@player_color, cloned_board.board) == false
+
+    false
+  end
 
   def iterate_through_pieces(pieces, board)
     pieces.each do |piece|
@@ -47,6 +90,14 @@ class Check # rubocop:disable Style/Documentation
       return true if sqr.piece != '  ' && sqr.piece.instance_of?(King) && sqr.piece.fg_color != @color
     end
     false
+  end
+
+  def alter_color(color)
+    if color == :black
+      :white
+    else
+      :black
+    end
   end
 
   def get_the_sqr_obj(board, coord)
