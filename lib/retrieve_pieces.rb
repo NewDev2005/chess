42c3e1f7230f698f4afeb7caa1b48dc85974bc97
krypto <clayjeansen3@gmail.com>
2026-01-28 19:09:03 +0530

    add #retrieve_opponent_pawns(gets all the pawns of the opponent

--- lib/retrieve_pieces.rb
+++ lib/retrieve_pieces.rb
@@ -13,4 +13,16 @@ module PieceRetrieval
     end
     pieces
   end
+
+  def retrieve_opponent_pawns(board, color)
+    pieces = []
+    board.each_value do |files|
+      files.each do |elem|
+        elem.each_value do |sqr|
+          pieces.push(sqr.piece) if sqr.piece != '  ' && sqr.piece.fg_color == color && sqr.piece.instance_of?(Pawn)
+        end
+      end
+    end
+    pieces
+  end
 end
