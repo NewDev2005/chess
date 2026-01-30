73f384da05939d225a247a0ac0bd40a16de5bc1d
krypto <clayjeansen3@gmail.com>
2026-01-30 16:48:06 +0530

    check if king and rook has moved previously and king is in check

---
+++ lib/castling.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative 'retrieve_pieces'
+require_relative 'check_mate'
+
+module Castling # rubocop:disable Style/Documentation
+  include PieceRetrieval
+
+  def king_side_castling_possible?(board, coord)
+    return unless king_in_check?(board, coord) == false
+
+    board = board.board
+    king_previously_not_moved?(board, coord) && rook_previously_not_moved?(board, coord)
+    # vacant_sqrs_between_king_and_rook?(coord)
+  end
+
+  def king_in_check?(board, coord)
+    king = get_the_piece(board.board, coord)
+    king_color = king.fg_color
+    check = Check.new(board)
+    check.in_check?(king_color, board)
+  end
+
+  def king_previously_not_moved?(board, coord)
+    king = get_the_piece(board, coord)
+    return true if king.previous_position.nil?
+
+    false
+  end
+
+  def rook_previously_not_moved?(board, coord)
+    rook = get_the_piece(board, coord)
+    return true if rook.previous_position.nil?
+
+    false
+  end
+end
+
+# castling conditions:
+# 1=> Neither the king nor the king has previously moved(given the condition the king is not in check).
+# 2=> The squares between the king and rook are vacant
+# 3=> The king doesn't leave, cross over and end up on a square being controlled by an enemy piece
