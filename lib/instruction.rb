95d3fdde2a4875c2160c888779759007f80ab617
krypto <clayjeansen3@gmail.com>
2026-01-31 15:46:08 +0530

    #declare_winner_message: add new line

--- lib/instruction.rb
+++ lib/instruction.rb
@@ -71,6 +71,7 @@ module GameInstruction # rubocop:disable Style/Documentation
 
   def declare_winner_message(name)
     print "</Congrats #{name}, you have won the game!>"
+    puts '\n'
   end
 
   def illegal_move_message
