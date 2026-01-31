cfd6f02ec4d2a281cdab97d4c2861bd556df437c
krypto <clayjeansen3@gmail.com>
2026-01-31 15:08:51 +0530

    identify queenside or kingside rook

--- lib/castling.rb
+++ lib/castling.rb
@@ -10,11 +10,12 @@ module Castling # rubocop:disable Style/Documentation
     return unless king_in_check?(board, coord) == false
 
     board = board.board
-    k_and_r_not_moved?(board, coord) && vacant_sqrs_in_kingside?(board, coord)
+    k_and_r_not_moved?(board, coord, 'kingside') && vacant_sqrs_in_kingside?(board, coord)
   end
 
-  def k_and_r_not_moved?(board, coord)
-    king_previously_not_moved?(board, coord) && rook_previously_not_moved?(board, coord)
+  def k_and_r_not_moved?(board, coord, rook_type)
+    color = get_the_piece(board, coord).fg_color
+    king_previously_not_moved?(board, coord) && rook_previously_not_moved?(board, rook_type, color)
   end
 
   def king_in_check?(board, coord)
@@ -31,11 +32,21 @@ module Castling # rubocop:disable Style/Documentation
     false
   end
 
-  def rook_previously_not_moved?(board, coord)
-    rook = get_the_piece(board, coord)
+  def rook_previously_not_moved?(board, rook_type, color)
+    rook_coord = kingiside_rook_coord(color) if rook_type == 'kingside'
+    sqr = get_the_sqr(board, rook_coord)
+    return false unless sqr.piece != '  ' && sqr.piece.instance_of?(Rook)
+
+    rook = sqr.piece
     return true if rook.previous_position.nil?
 
     false
+  end
+
+  def kingiside_rook_coord(rook_color)
+    return 'hi' if rook_color == :white
+
+    'h8'
   end
 
   def vacant_sqrs_in_kingside?(board, coord)
@@ -59,8 +70,3 @@ module Castling # rubocop:disable Style/Documentation
     sqrs
   end
 end
-
-# castling conditions:
-# 1=> Neither the king nor the king has previously moved(given the condition the king is not in check).
-# 2=> The squares between the king and rook are vacant
-# 3=> The king doesn't leave, cross over and end up on a square being controlled by an enemy piece
