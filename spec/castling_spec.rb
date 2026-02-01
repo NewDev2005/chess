8f07a4e55f0ce0b2d3b5c880831fdde4f536d17a
krypto <clayjeansen3@gmail.com>
2026-02-01 18:30:49 +0530

    test cases for when the rook has previously moved

--- spec/castling_spec.rb
+++ spec/castling_spec.rb
@@ -3,7 +3,7 @@
 require_relative 'spec_helper'
 require_relative '../lib/play_game'
 
-describe 'Castling' do
+describe 'Castling' do # rubocop:disable Metrics/BlockLength
   it 'kingside castling move is available when sqrs between rook and king are vacant and king is not in check' do
     game = PlayGame.new
     game.board.create_board
@@ -50,6 +50,26 @@ describe 'Castling' do
     game.execute_move(color: :black, origin: 'f8', target: 'd6')
     game.execute_move(color: :black, origin: 'e8', target: 'f8')
     game.execute_move(color: :black, origin: 'f8', target: 'e8')
+
+    king = game.get_the_piece(board, 'e8')
+    king.previous_position
+    legal_kingside_castling_move = king.legal_castling_moves(board_obj)
+
+    expect(legal_kingside_castling_move).to_not include('g8')
+  end
+
+  it 'when the rook has previously moved castiling is not possible' do
+    game = PlayGame.new
+    game.board.create_board
+    board = game.board.board
+    board_obj = game.board
+
+    game.execute_move(color: :white, origin: 'e2', target: 'e4')
+    game.execute_move(color: :black, origin: 'f7', target: 'f6')
+    game.execute_move(color: :black, origin: 'g8', target: 'h6')
+    game.execute_move(color: :black, origin: 'f8', target: 'd6')
+    game.execute_move(color: :black, origin: 'h8', target: 'g8')
+    game.execute_move(color: :black, origin: 'g8', target: 'h8')
 
     king = game.get_the_piece(board, 'e8')
     king.previous_position
