09ce1916b22b5d38fbff90d322501d08b3dc3b5b
krypto <clayjeansen3@gmail.com>
2026-02-01 18:01:25 +0530

    test kingside castling moves when the king is in check and has moved previously

---
+++ spec/castling_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+require_relative '../lib/play_game'
+
+describe 'Castling' do
+  it 'kingside castling move is available when sqrs between rook and king are vacant and king is not in check' do
+    game = PlayGame.new
+    game.board.create_board
+    board = game.board.board
+    board_obj = game.board
+
+    game.execute_move(color: :white, origin: 'e2', target: 'e4')
+    game.execute_move(color: :white, origin: 'f1', target: 'd3')
+    game.execute_move(color: :white, origin: 'g1', target: 'h3')
+
+    king = game.get_the_piece(board, 'e1')
+    legal_kingside_castling_move = king.legal_castling_moves(board_obj)
+
+    expect(legal_kingside_castling_move).to include('g1')
+  end
+
+  it 'kingside castling not possible when the king is in check' do
+    game = PlayGame.new
+    game.board.create_board
+    board = game.board.board
+    board_obj = game.board
+
+    game.execute_move(color: :white, origin: 'e2', target: 'e4')
+    game.execute_move(color: :black, origin: 'f7', target: 'f6')
+    game.execute_move(color: :black, origin: 'g8', target: 'h6')
+    game.execute_move(color: :black, origin: 'f8', target: 'd6')
+    game.execute_move(color: :white, origin: 'd1', target: 'h5')
+
+    king = game.get_the_piece(board, 'e8')
+    legal_kingside_castling_move = king.legal_castling_moves(board_obj)
+
+    expect(legal_kingside_castling_move).to_not include('g8')
+  end
+
+  it 'when the king has previously has moved castling is not possible' do
+    game = PlayGame.new
+    game.board.create_board
+    board = game.board.board
+    board_obj = game.board
+
+    game.execute_move(color: :white, origin: 'e2', target: 'e4')
+    game.execute_move(color: :black, origin: 'f7', target: 'f6')
+    game.execute_move(color: :black, origin: 'g8', target: 'h6')
+    game.execute_move(color: :black, origin: 'f8', target: 'd6')
+    game.execute_move(color: :black, origin: 'e8', target: 'f8')
+    game.execute_move(color: :black, origin: 'f8', target: 'e8')
+
+    king = game.get_the_piece(board, 'e8')
+    king.previous_position
+    legal_kingside_castling_move = king.legal_castling_moves(board_obj)
+
+    expect(legal_kingside_castling_move).to_not include('g8')
+  end
+end
